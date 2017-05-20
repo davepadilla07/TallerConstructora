0781b875c08ed112e36e8f31c155fa65f81612a1
Dave <ing.dave@hotmail.es>
2017-05-20 01:18:07 -0500

    arreglar base datos

--- app/src/main/java/com/example/david/tallerconstructora/ApartamentosCaracteristica.java
+++ app/src/main/java/com/example/david/tallerconstructora/ApartamentosCaracteristica.java
@@ -1,13 +1,42 @@
 package com.example.david.tallerconstructora;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ApartamentosCaracteristica extends AppCompatActivity {
+    private TextView cajaResultado;
+    private String aux;
+    private Resources res;
+    private ArrayList<Apartamento> apartamentos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apartamentos_caracteristica);
+
+        cajaResultado=(TextView)findViewById(R.id.txtResultado1);
+        res=this.getResources();
+
+    }
+
+    public void reporte(View v){
+        int cont=0;
+        String balcon,sombra;
+
+
+        apartamentos=Datos.traerApartamentos(getApplicationContext());
+        for (int i = 0; i < apartamentos.size(); i++) {
+            if ((apartamentos.get(i).getCaracteristica()==res.getString(R.string.balcon))&&(apartamentos.get(i).getCaracteristica()==res.getString(R.string.sombra))){
+                cont=cont+1;
+            }
+            aux=res.getString(R.string.apartamento_caracteristica)+" "+cont;
+            cajaResultado.setText(aux);
+        }
+
     }
 }
